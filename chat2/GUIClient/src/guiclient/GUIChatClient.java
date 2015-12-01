c83de84cdd4a2033b8b5c41a16d8cec8a5014ee0
Ahmad Zaenal Mustofa <mustofaahmad29@gmail.com>
2015-12-01 10:47:39 +0700

    sudah bisa chatting

--- chat2/GUIClient/src/guiclient/GUIChatClient.java
+++ chat2/GUIClient/src/guiclient/GUIChatClient.java
@@ -11,6 +11,8 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -168,6 +170,8 @@ public class GUIChatClient extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         viewTextArea = new javax.swing.JTextArea();
         TmblConnect = new javax.swing.JButton();
+        postTextField = new javax.swing.JTextField();
+        TmblKirim = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -182,6 +186,13 @@ public class GUIChatClient extends javax.swing.JFrame {
             }
         });
 
+        TmblKirim.setText("Send");
+        TmblKirim.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TmblKirimActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -189,25 +200,32 @@ public class GUIChatClient extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(54, 54, 54)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(username1)
+                        .addGap(18, 18, 18)
+                        .addComponent(TmblConnect))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(username1, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
-                        .addComponent(TmblConnect)
-                        .addGap(56, 56, 56))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addComponent(postTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(TmblKirim)
+                        .addGap(0, 11, Short.MAX_VALUE)))
+                .addGap(56, 56, 56))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(username1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(username1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(TmblConnect))
                 .addGap(35, 35, 35)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(135, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 142, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(postTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(TmblKirim, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(29, 29, 29))
         );
 
         pack();
@@ -219,6 +237,17 @@ public class GUIChatClient extends javax.swing.JFrame {
         start();
         viewTextArea.setText("Selamat datang ");
     }//GEN-LAST:event_TmblConnectActionPerformed
+
+    private void TmblKirimActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TmblKirimActionPerformed
+        try {
+            // TODO add your handling code here:
+            String message = "postText~" + username + "~" + postTextField.getText() + "~all~\n";
+            output.writeObject(message);
+            postTextField.setText("");
+        } catch (IOException ex) {
+            Logger.getLogger(GUIChatClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_TmblKirimActionPerformed
 
     /**
      * @param args the command line arguments
@@ -257,7 +286,9 @@ public class GUIChatClient extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton TmblConnect;
+    private javax.swing.JButton TmblKirim;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField postTextField;
     private javax.swing.JTextField username1;
     private javax.swing.JTextArea viewTextArea;
     // End of variables declaration//GEN-END:variables
