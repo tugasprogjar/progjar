590ed85bf05126798f10783b7f298b8789807594
unknown <mustofaahmad29@gmail.com>
2016-01-05 08:43:22 -0800

    menambahkan percobaan 2

---
+++ FP/Coba2/MangiareClient/src/mangiareclient/MangiareClient.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mangiareclient;
+
+
+import java.io.*;
+import java.net.*;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+/**
+ *
+ * @author Ilham
+ */
+public class MangiareClient {
+
+    public int port;
+    public String serverAddr;
+    public Socket socket;
+    public ObjectInputStream In;
+    public ObjectOutputStream Out;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+}
